abd11bd030c20e11128dea423153d57491d3cc35
balaji <balajim@incubxperts.com>
2023-06-23 19:53:27 +0530

    added angular localize support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { HighlightSalaryDirective } from './directives/highlight-salary.directive';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'; // Import locale data for French
+import localeDe from '@angular/common/locales/de'; // Import locale data for German
 
+// Register the locale data
+registerLocaleData(localeFr);
+registerLocaleData(localeDe);
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +23,9 @@ import { HighlightSalaryDirective } from './directives/highlight-salary.directiv
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-US' } // Default to English (United States)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -7,7 +7,16 @@ import { EmployeeService } from 'src/app/services/employee.service';
 })
 export class EmployeeComponent {
   employees!: any[];
-constructor(private employeeService: EmployeeService) { }
+  locale: string='en-US'
+constructor(private employeeService: EmployeeService) {
+  debugger
+  const userLanguage = navigator.language; // Get the user's language from the browser
+    if (userLanguage.startsWith('fr')) {
+      this.locale = 'fr-FR'; // Set the locale to French
+    } else if (userLanguage.startsWith('de')) {
+      this.locale = 'de-DE'; // Set the locale to German
+    }
+ }
   ngOnInit() {
     this.employees = this.employeeService.getEmployees();
   }
